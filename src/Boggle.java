934da96e885dc15c7dcbcc4ac7e988a12831beb1
jacobldot <jacob.lowe@menloschool.org>
2026-01-09 09:29:51 -0800

    a

--- src/Boggle.java
+++ src/Boggle.java
@@ -5,10 +5,19 @@ public class Boggle {
 
     public static String[] findWords(char[][] board, String[] dictionary) {
 
-        ArrayList<String> goodWords = new ArrayList<String>();
+        TST dictList = new TST();
+        for (String s : dictionary) {
+            dictList.insert(s, 1);
+        }
 
-        // TODO: Complete the function findWords(). Add all words that are found both on the board
-        //  and in the dictionary.
+        ArrayList<String> goodWords = new ArrayList<>();
+        boolean[][] visited = new boolean[board.length][board[0].length];
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs(board, i, j, "", visited, dictList, goodWords);
+            }
+        }
 
         // Convert the list into a sorted array of strings, then return the array.
         String[] sol = new String[goodWords.size()];
@@ -16,4 +25,26 @@ public class Boggle {
         Arrays.sort(sol);
         return sol;
     }
+
+    public static void dfs(char[][] board, int i, int j, String prefix, boolean[][] visited, TST dictList, ArrayList<String> goodWords) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length)  return;
+        if (visited[i][j]) return;
+
+        visited[i][j] = true;
+
+        prefix = prefix + board[i][j];
+
+        if (dictList.lookup(prefix) == 1 && !goodWords.contains(prefix)) {
+            goodWords.add(prefix);
+        }
+
+        dfs(board, i - 1, j, prefix, visited, dictList, goodWords);
+        dfs(board, i + 1, j, prefix, visited, dictList, goodWords);
+        dfs(board, i, j - 1, prefix, visited, dictList, goodWords);
+        dfs(board, i, j + 1, prefix, visited, dictList, goodWords);
+
+        visited[i][j] = false;
+    }
+
+
 }
